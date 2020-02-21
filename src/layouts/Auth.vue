5c196d4c19fb4b9e8cfc953c21b370380b216908
Fernando dos Santos <nando4803@gmail.com>
2020-02-21 17:15:03 +0100

    Authetication

---
+++ src/layouts/Auth.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'Auth'
+  // eslint-disable-next-line semi
+};
+</script>
