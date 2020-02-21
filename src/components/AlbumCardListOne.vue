03e8ea9b710ef610b7a5ec300321e5e6e93904e4
Fernando dos Santos <nando4803@gmail.com>
2020-02-21 15:31:46 +0100

    AlbumCardListOne e AlbumCardListTwo.

---
+++ src/components/AlbumCardListOne.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row>
+    <v-col cols="6" md="2" v-for="item of 4" :key="item">
+      <album-card-one></album-card-one>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'AlbumCardListOne',
+
+  components: {
+    AlbumCardOne: () => import('./AlbumCardOne.vue')
+  }
+  // eslint-disable-next-line semi
+};
+</script>
---
+++ src/components/AlbumCardListTwo.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row>
+    <v-col cols="6" md="3" v-for="item of 5" :key="item">
+      <album-card-two></album-card-two>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'AlbumCardListTwo',
+
+  components: {
+    AlbumCardTwo: () => import('./AlbumCardTwo.vue')
+  }
+  // eslint-disable-next-line semi
+};
+</script>
