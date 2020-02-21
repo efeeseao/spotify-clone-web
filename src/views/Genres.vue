647cb41ca34ed768d88e46cbc74f3c25b434f41f
Fernando dos Santos <nando4803@gmail.com>
2020-02-21 18:53:57 +0100

    Genre

---
+++ src/views/Genres.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-row>
+      <v-col>
+        <v-row class="align-start">
+          <v-col>
+            <h1 class="headline font-weight-bold">Genêros</h1>
+          </v-col>
+        </v-row>
+
+        <genre-card-list :total="10"></genre-card-list>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Genre',
+
+  components: {
+    GenreCardList: () => import('../components/GenreCardList.vue')
+  }
+  // eslint-disable-next-line semi
+};
+</script>
