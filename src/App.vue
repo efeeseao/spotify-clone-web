48dc9634288b31ccbff7cc5ab394324efb908c96
Fernando dos Santos <nando4803@gmail.com>
2020-02-21 11:34:51 +0100

    Inicializando o App e importando no arquivo main.

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,26 @@
-<template></template>
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+  // eslint-disable-next-line semi
+};
+</script>
+
+<style>
+a {
+  color: inherit !important;
+  text-decoration: none !important;
+}
+
+.albumCard,
+.artistCard,
+.genreCard {
+  border-radius: 5px !important;
+  cursor: pointer;
+}
+</style>
