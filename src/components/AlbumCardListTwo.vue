7cc2c47559311eb24b9794eff4794243b201628d
Fernando dos Santos <nando4803@gmail.com>
2020-02-21 19:32:16 +0100

    Fix problem

--- src/components/AlbumCardListTwo.vue
+++ src/components/AlbumCardListTwo.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row>
-    <v-col cols="6" md="3" v-for="item of 5" :key="item">
+    <v-col cols="6" md="3" v-for="item of 8" :key="item">
       <album-card-two></album-card-two>
     </v-col>
   </v-row>
