7b2b6f365433aaf11f8a37de424792c2057bed1d
Fernando dos Santos <nando4803@gmail.com>
2020-02-21 12:21:23 +0100

    Layout

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app>
+      <div class="pa-5">
+        <h1>
+          <router-link to="/">Spotify-web</router-link>
+        </h1>
+      </div>
+      <v-list>
+        <v-list-item v-for="item in menu" :key="item.title" dense link :to="item.to">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app class="elevation-0" dense>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    </v-app-bar>
+
+    <v-content class="pb-12">
+      <router-view class="pa-5" style="padding-bottom: 140px"></router-view>
+    </v-content>
+
+    <v-footer class="elevation-10" app fixed style="z-index:10">
+      <player></player>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+
+  components: {
+    Player: () => import('@/components/Palyer.vue')
+  },
+
+  data: () => ({
+    drawer: true,
+
+    menu: [
+      {
+        icon: 'mdi-home',
+        title: 'Inicio',
+        to: '/browse'
+      },
+      {
+        icon: 'mdi-magnify',
+        title: 'Buscar',
+        to: {
+          name: 'Search'
+        }
+      }
+    ]
+  })
+  // eslint-disable-next-line semi
+};
+</script>
