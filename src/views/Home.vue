e4efa03900cef3c6c40ac09cd60fbb10f2947079
Fernando dos Santos <nando4803@gmail.com>
2020-02-21 12:35:58 +0100

    Home

---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container id="home" fluid>
+    <v-row>
+      <v-col class="py-0" cols="auto">
+        <router-link
+          active-class="font-weight-bold"
+          class="white--text subtitle-2"
+          :to="{ name: 'featured '}"
+        >Destaque</router-link>
+      </v-col>
+      <v-col class="py-0" cols="auto">
+        <router-link
+          active-class="font-weight-bold"
+          class="white--text subtitle-2"
+          :to="{ name: 'genres' }"
+        >Generos</router-link>
+      </v-col>
+      <v-col>
+        <router-link
+          active-class="font-weight-bold"
+          class="white--text subtitle-2"
+          :to="{ name: 'discover' }"
+        >Descobrir</router-link>
+      </v-col>
+    </v-row>
+
+    <router-view></router-view>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+  // eslint-disable-next-line semi
+};
+</script>
