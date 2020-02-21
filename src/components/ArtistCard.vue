a700a129d2aeb0cddb9eb45030fa071f4b27c82c
Fernando dos Santos <nando4803@gmail.com>
2020-02-21 16:24:52 +0100

    ArtistCard e ArtistCardList.

---
+++ src/components/ArtistCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-hover v-slot:default="{ hover }">
+    <v-card class="artistCard" flat>
+      <v-card-text>
+        <v-img class="elevation-10" src style="border-radius: 50%"></v-img>
+      </v-card-text>
+
+      <v-card-tex class="pt-0">
+        <p class="body-2 font-weight-medium ma-0 text-truncate white--text">
+          <router-link
+            :to="{
+              name: 'artist',
+              params: {
+                id: 1
+              }
+            }"
+          >Queen</router-link>
+        </p>
+        <p class="caption font-weight-light mb-4 text-truncate">Artista</p>
+        <div style="position: absolute; right: 10px; bottom: 10px" v-show="hover">
+          <v-icon size="35">mdi-play-circle-outline</v-icon>
+        </div>
+      </v-card-tex>
+    </v-card>
+  </v-hover>
+</template>
---
+++ src/components/ArtistCardList.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row>
+    <v-col cols="6" sm="3" v-for="item of 4" :key="item">
+      <artist-card></artist-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'ArtisCardList',
+
+  components: {
+    ArtistCard: () => import('./ArtistCard.vue')
+  }
+  // eslint-disable-next-line semi
+};
+</script>
