a746997aa9aeeef23ea01c3004795cb410c78101
Fernando dos Santos <nando4803@gmail.com>
2020-02-21 14:50:41 +0100

    Queue

---
+++ src/views/Queue.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container id="playlist" fluid>
+    <v-row>
+      <v-col>
+        <h1>Fila de Reprodução</h1>
+        <v-subheader>ATUAL</v-subheader>
+        <song-list :total="1"></song-list>
+        <v-list style="background: none">
+          <v-subheader>NEXT</v-subheader>
+          <song-list></song-list>
+        </v-list>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Queue',
+
+  components: {
+    SongList: () => import('../components/SongListOne.vue')
+  }
+  // eslint-disable-next-line semi
+};
+</script>
