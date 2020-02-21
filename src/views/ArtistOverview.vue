194ee75bb37d19793e8ce1ee8ef1048cb96cf445
Fernando dos Santos <nando4803@gmail.com>
2020-02-21 18:16:09 +0100

    ArtistOverview

---
+++ src/views/ArtistOverview.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-row>
+      <v-col>
+        <v-row class="align-start">
+          <v-col>
+            <h1 class="headline font-weight-bold">Popular</h1>
+          </v-col>
+        </v-row>
+
+        <song-list-two></song-list-two>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-row class="align-start">
+          <v-col>
+            <h1 class="headline font-weight-bold">Albums</h1>
+          </v-col>
+        </v-row>
+        <album-card-list-two></album-card-list-two>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArtistOverview',
+
+  components: {
+    AlbumCardListTwo: () => import('../components/AlbumCardListTwo.vue'),
+    SongListTwo: () => import('../components/SongListTwo.vue')
+  }
+  // eslint-disable-next-line semi
+};
+</script>
