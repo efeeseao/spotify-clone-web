613f5a1751d6d646c4df9e7d918dde7b96fef616
Fernando dos Santos <nando4803@gmail.com>
2020-02-21 18:01:15 +0100

    Artist e Album views

---
+++ src/views/Album.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-container id="album" fluid>
+    <v-row>
+      <v-col class="pa-10 pa-sm-4 py-sm-0 pa-md-8 py-md-0 py-0" cols="12" sm="5" md="4">
+        <div>
+          <v-hover v-slot:default="{ hover }">
+            <v-img
+              class="elevation-10"
+              src="http://www.queenonline.com/global/assets/modules/site/images/music/GREATEST_HITS_V2.jpg"
+            >
+              <v-overlay absolute opacity="0.5" :value="hover">
+                <v-icon size="90">mdi-play-circle-outline</v-icon>
+              </v-overlay>
+            </v-img>
+          </v-hover>
+          <div class="text-center mt-4 mb-0">
+            <p class="headline font-weight-bold ma-0 mb-1" style="line-height: 1.5rem">Killer Queen</p>
+            <p class="subtitle-1 mb-4">
+              <router-link
+                :to="{
+                    name: 'artist',
+                    params: {
+                      id: 1
+                    }
+                  }"
+              >Queen</router-link>
+            </p>
+            <p class="caption">1981 - 14 MÚSICAS</p>
+          </div>
+        </div>
+      </v-col>
+      <v-col class="pt-0" cols="12" md="8">
+        <v-hover v-for="song in songs" :key="song" v-slot:default="{ hover }">
+          <v-list-item link two-line>
+            <v-list-item-content class="py-0">
+              <v-row class="align-center">
+                <v-col class="py-0" cols="auto">
+                  <v-icon>{{ hover ? 'mdi-play' : 'mdi-music-note-outline' }}</v-icon>
+                </v-col>
+                <v-col>
+                  <v-list-item-title>
+                    <v-list-item-title>{{ song }}</v-list-item-title>
+                    <v-list-item-subtitle>Queen</v-list-item-subtitle>
+                  </v-list-item-title>
+                </v-col>
+                <v-col cols="auto">3:01</v-col>
+              </v-row>
+            </v-list-item-content>
+          </v-list-item>
+        </v-hover>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+// eslint-disable-next-line semi
+export default {
+  name: 'Album',
+
+  data: () => ({
+    songs: [
+      'Bohemian Rhapsody',
+      'Another One Bites the Dust',
+      'Killer Queen',
+      'Fat Bottomed Girls',
+      'Bicycle Race',
+      'You are my Best Friend'
+    ]
+  })
+  // eslint-disable-next-line semi
+};
+</script>
---
+++ src/views/Artist.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container>
+    <v-row class="pa-10 pb-0 artist-header align-end">
+      <v-col>
+        <span class="caption">210,541 OUVINTES POR MÊS</span>
+        <h1 class="display-3 font-weight-bold">Queen</h1>
+      </v-col>
+
+      <v-col class="pb-0" cols="12">
+        <v-tabs background-color="transparent" color="white">
+          <v-tab
+            :to="{
+              name: 'artist',
+              params: {
+                id: $route.params.id
+              }
+            }"
+          >Visão Geral</v-tab>
+          <v-tab
+            to="{
+              name: 'about-artist',
+              params: {
+                id: $route.params.id
+              }
+            }"
+          >Sobre</v-tab>
+        </v-tabs>
+      </v-col>
+    </v-row>
+
+    <router-view class="px-7"></router-view>
+  </v-container>
+</template>
+
+<style scoped>
+#artist {
+  padding: 0 !important;
+}
+
+.artist-header {
+  background-image: linear-gradient(
+      10deg,
+      rgba(48, 48, 48, 1),
+      rgba(0, 0, 0, 0)
+    ),
+    url(https://images.cdn.veronicamagazine.nl/pVwUIOP2luwE3FeR8ayy7rVMYUs=/0x0:1200x675/890x0/smart/veronicamagazine.nl/s3fs-public/main_media/p01l6rj1.jpg?itok=VHMh1jY6);
+  background-position: 50%;
+  background-size: cover;
+  height: 320px;
+}
+</style>
